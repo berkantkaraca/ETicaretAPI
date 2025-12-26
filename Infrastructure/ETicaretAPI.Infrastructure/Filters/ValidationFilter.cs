19595a97d54fd9a823100fb53b68da972c1d8d8c
Berkant Karaca <berkantkaracatr@gmail.com>
2025-12-26 15:42:43 +0300

    Fluent Validation İle ValidationFilter

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ETicaretAPI.Infrastructure.Filters
+{
+    /// <summary>
+    /// Action'a gelen model doğrulamasını yapan filter.
+    /// </summary>
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                       .Where(x => x.Value.Errors.Any())
+                       .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage))
+                       .ToArray();
+
+                context.Result = new BadRequestObjectResult(errors);
+                return;
+            }
+
+            await next();
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,7 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
 
 namespace ETicaretAPI.API
 {
@@ -16,7 +19,12 @@ namespace ETicaretAPI.API
                 policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200");
             }));
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<ValidationFilter>(); //EF Core Validasyonlarýný devre dýþý býrakýp kendi filterýmýzý ekliyoruz
+            })
+                .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>()) //Fluent Validationlarý ekler
+                .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true); //EF Core Validasyonlarýný devre dýþý býrakýr
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
