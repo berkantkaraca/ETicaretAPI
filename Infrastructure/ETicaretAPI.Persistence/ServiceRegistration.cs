4c80dcef2e7103d529f1e0d48ba9e1228658208b
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-18 12:31:48 +0300

    Rol Yönetim Sayfasının Oluşturulması

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -56,6 +56,7 @@ namespace ETicaretAPI.Persistence
             services.AddScoped<IInternalAuthentication, AuthService>();
             services.AddScoped<IBasketService, BasketService>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IRoleService, RoleService>();
         }
     }
 }
