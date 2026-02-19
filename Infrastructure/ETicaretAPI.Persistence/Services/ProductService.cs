51e14fb9df12ab412e10119acf87961e60b1ffa3
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-19 08:40:20 +0300

    QrCode Okuma ve Ürün Stock Bilgisi Güncelleme

--- Infrastructure/ETicaretAPI.Persistence/Services/ProductService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Services/ProductService.cs
@@ -9,10 +9,13 @@ namespace ETicaretAPI.Persistence.Services
     {
         readonly IProductReadRepository _productReadRepository;
         readonly IQRCodeService _qrCodeService;
-        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qrCodeService)
+        readonly IProductWriteRepository _productWriteRepository;
+
+        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qrCodeService, IProductWriteRepository productWriteRepository)
         {
             _productReadRepository = productReadRepository;
             _qrCodeService = qrCodeService;
+            _productWriteRepository = productWriteRepository;
         }
 
         public async Task<byte[]> QrCodeToProductAsync(string productId)
@@ -33,6 +36,18 @@ namespace ETicaretAPI.Persistence.Services
             string plainText = JsonSerializer.Serialize(plainObject);
 
             return _qrCodeService.GenerateQRCode(plainText);
+        }
+
+        public async Task StockUpdateToProductAsync(string productId, int stock)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(productId);
+
+            if (product == null)
+                throw new Exception("Product not found");
+
+            product.Stock = stock;
+
+            await _productWriteRepository.SaveAsync();
         }
     }
 }
